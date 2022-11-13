73477ad61c0f5566a76eb499d3cef94a5f481ab0
Leandro Biesek <l.biesek@gmail.com>
2022-11-12 22:47:05 -0300

    Inicio

---
+++ src/main/java/com/api/piorfilme/PiorfilmeApplication.java
@@ -0,0 +1,13 @@
+package com.api.piorfilme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiorfilmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiorfilmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/piorfilme/PiorfilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.piorfilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiorfilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
